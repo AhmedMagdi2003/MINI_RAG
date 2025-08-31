9c0d45d738e9a0892535db8a11c5cde2b27b4f50
AhmedMagdi2003 <amg300571@gmail>
2025-08-31 22:54:57 +0300

    docker intiallize

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     FILE_ALLOWED_EXTENTION : str
     FILE_MAX_SIZE : int
     FILE_DEFAULT_CHUNK_SIZE: int
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
+
 
     class Config:
         env_file = ".env"
