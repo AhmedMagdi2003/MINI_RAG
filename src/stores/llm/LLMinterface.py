033596afa75154172bfdbba8d179aaaccef843f9
AhmedMagdi2003 <amg300571@gmail>
2026-03-22 00:46:23 +0200

    OpenAI Provider

---
+++ src/stores/llm/LLMinterface.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class LLMinterface(ABC):
+
+    @abstractmethod
+    def set_generating_model(self,model_id:str):
+        pass
+
+    @abstractmethod
+    def set_embedding_model(self,model_id:str, embedding_size:int):
+        pass
+
+    @abstractmethod
+    def generate_text(self, prompt: str,chat_history: list = [] ,temperature: float=None, max_output_tokens: int=None ):
+        pass
+
+    @abstractmethod
+    def embed_text(self, text: str, documnet_type: str = None):
+        pass
+
+    @abstractmethod
+    def construct_prompt(self, prompt:str, role: str):
+        pass
